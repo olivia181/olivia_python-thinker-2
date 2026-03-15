fde10fa33572ee0196632598cc09b6716debbbed
olivia181 <livie.ning@gmail.com>
2026-03-15 09:30:37 +0000

    lesson 7

--- CT07_07/lesson7.py
+++ CT07_07/lesson7.py
@@ -73,8 +73,123 @@
 # print(fruits[index:])
 
 
-nums = [4,7,2,9,5,8,1,6,3] 
-# Using slicing ONLY (no loops), 
-# extract all numbers at EVEN positions.
-print(nums[2:])
-print(nums[:9:])
+#nums = [4,7,2,9,5,8,1,6,3] 
+# # Using slicing ONLY (no loops), 
+# # extract all numbers at EVEN positions.
+
+
+
+
+# ## Task 4: Splitting a List in Half
+# You have been tasked to divide the basket of fruits into
+# 2 equal halves. Given a list of even length, split it
+# into 2 equal halves.
+
+# fruits = ["Apple", "Banana", "Cherry", "Durian", "Elderberry", "Figs"]
+
+# 1. Find the midpoint of the list.
+# 2. Split the list into 2 halves using slicing.
+# 3. Print both halves.
+
+# middle = len(fruits)//2
+
+# print(fruits[:middle])
+# print(fruits[middle:])
+
+# ---------------------------------------------------------------
+
+# ## Task 5: Identifying Common Elements in Lists
+# You have been given 2 lists of fruits. However, there might be
+# duplicates. Your job is to identify and print the common fruits
+# between them.
+
+# list1 = ["Apple", "Banana", "Cherry", "Durian"]
+# list2 = ["Cherry", "Durian", "Elderberry", "Figs"]
+
+# 1. Create an empty list named 'common'
+# 2. Using 'for' loops, append common elements into 'common'
+# 3. Print the common elements
+
+# common_list = []
+# for fruit in list1:
+#     for fruit2 in list2:
+#         if fruit == fruit2:
+#             common_list.append(fruit)
+# print(common_list)
+# ---------------------------------------------------------------
+
+# ## Task 6: Merging Lists Unique Items
+# You have been given 2 lists of fruits that contains duplicates.
+# Your task is to merge the 2 lists into a new list that contain
+# no duplicates.
+
+# list1 = ["Apple", "Banana", "Cherry", "Cherry"]
+# list2 = ["Cherry", "Durian", "Durian", "Figs"]
+
+# 1. Create an empty list named 'unique'
+# 2. Using 'for' loops, append unique elements into 'unique'
+# 3. Print the unique elementscommon_list = []
+
+# unique_list = []
+# for fruit in list1 + for fruit in list2:
+#     if fruit not in unique_list:
+#         unique_list.append(fruit)
+# print(unique_list)
+
+
+# ---------------------------------------------------------------
+
+# ## Task 7: Merging Lists with Conditions
+# You have been given the index number of 2 groups of students.
+# However, only students with even index number is allowed
+# to come into the room. Create a Python script that will
+# merge the 2 lists, including only the elements that are
+# even from both.
+
+# list1 = [1, 2, 3, 4]
+# list2 = [5, 6, 7, 8]
+
+# # 1. Merge the lists.
+# # 2. Print the new list.
+
+# list3 = []
+# for number in list1 + list2:
+#     if number %2 ==0:
+#         list3.append(number)
+# print(list3)
+
+# ---------------------------------------------------------------
+
+# ## Task 8: Nested List Splitting
+# You are given a nested list of 3 groups of students that
+# are each seated in a pair. However, you want to unpack
+# the nested lists in order to have a list of all students.
+
+# nested_list = [[1, 2], [3, 4], [5, 6]]
+
+# 1. Use a loop or list comprehension to flatten the list.
+# 2. Print the flattened list.
+
+# list_flat = []
+# for list1 in nested_list:
+#     for num in list1:
+#         list_flat.append(num)
+# print(list_flat)
+# ---------------------------------------------------------------
+
+# ## Task 9: Partitioning a List into Smaller Lists
+# You have been tasked to divide a class of 9 students
+# into groups of 3.
+
+# Given a list and a size, split the list into multiple
+# sub-lists where each sub-list is of the given size.
+
+# students = [1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12]
+# size = 2
+# nested_student_list = []
+# 1. Use a loop to create sub-lists of the specified size.
+# 2. Print the sub-lists.
+# in range(0, len(students), size)
+# for i in range(0, len(students), size):
+#     nested_student_list.append(students[i:i+size])
+# print(nested_student_list)
